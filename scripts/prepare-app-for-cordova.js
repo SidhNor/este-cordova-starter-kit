527f8ec0d3a71f7de4e03af4eaffb51843a2b672
Gleb Godonoga <gleb.godonoga@gmail.com>
2016-03-17 12:05:27 +0000

    Initial import

---
+++ scripts/prepare-app-for-cordova.js
@@ -0,0 +1,21 @@
+module.exports = function(ctx) {
+
+  var exec = require('child_process').exec,
+    util = require('util'),
+    deferral = ctx.requireCordovaModule('q').defer();
+
+  function puts(error, stdout, stderr) {
+    if (error)
+      deferral.reject(error)
+    util.puts(stdout)
+  }
+  var lt = exec('gulp to-html', puts)
+  lt.stdout.pipe(process.stdout);
+  lt.stderr.pipe(process.stderr);
+  lt.on('exit', function (code) {
+    util.puts('Gulp exited with exit code ' + code);
+    deferral.resolve()
+  });
+
+  return deferral.promise;
+};
